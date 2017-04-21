f965a1dc7fb99d7011e1396bd9937ceef73fa0b3
ORPHANED <cran@R-project.org>
2017-04-21 13:55:21 +0000

    version 0.1.2

--- tests/test_all.r
+++ tests/test_all.r
@@ -2,7 +2,8 @@ library('Metrics')
 library('RUnit')
 
 test.suite <- defineTestSuite("Metrics",
-                              dirs = file.path("inst/tests"),
+                              dirs = file.path(path.package(package="Metrics"),
+					       "tests"),
                               testFileRegexp = '^.+\\.r')
 
 test.result <- runTestSuite(test.suite)
